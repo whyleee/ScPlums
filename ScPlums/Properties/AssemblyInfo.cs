07c1acbae3442bf550c8808e95b3523d7162341c
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-07-31 18:55:24 +0300

    Added nuspec

--- ScPlums/Properties/AssemblyInfo.cs
+++ ScPlums/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General info about the product
 [assembly: AssemblyTitle("ScPlums")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ciklum")]
+[assembly: AssemblyDescription("Tasty plums in different Sitecore places")]
+[assembly: AssemblyCompany("Pavel Nezhencev (Creuna)")]
 [assembly: AssemblyProduct("ScPlums")]
-[assembly: AssemblyCopyright("Copyright © ciklum 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Pavel Nezhencev (Creuna) 2014")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e0d6b85c-5562-470c-b2b5-d7e8718f9a06")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Product version
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
