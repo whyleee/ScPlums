0e747e8be5b1eaa6201bd80c5f429a00465ace13
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-09-04 19:50:01 +0300

    Added page editor fix for News Mover plugin (redirect user after item moved)

---
+++ ScPlums/Fixes/NewsMover/RedirectWebEdit.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+using Sitecore.Links;
+using Sitecore.Pipelines;
+using Sitecore.Sites;
+using Sitecore.Web;
+using Sitecore.Web.UI.Sheer;
+
+namespace ScPlums.Fixes.NewsMover
+{
+    public class RedirectWebEdit
+    {
+        public void Process(PipelineArgs args)
+        {
+            if (Sitecore.Context.ClientPage.ClientRequest.Parameters != "webedit:save")
+            {
+                return;
+            }
+            if (args.GetType().FullName != "Sitecore.Sharedsource.NewsMover.Pipelines.MoveCompletedArgs")
+            {
+                return;
+            }
+
+            var articleItem = (Item) args.GetType().GetProperty("Article").GetValue(args);
+            var site = SiteContext.GetSite(WebEditUtil.SiteName);
+
+            using (new SiteContextSwitcher(site))
+            {
+                using (new LanguageSwitcher(articleItem.Language))
+                {
+                    var targetUrl = LinkManager.GetItemUrl(articleItem);
+                    SheerResponse.Eval("scNavigate(\"" + targetUrl + "\")");
+                }
+            }
+        }
+    }
+}
