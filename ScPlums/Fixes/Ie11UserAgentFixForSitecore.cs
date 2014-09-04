d00f316bec85d00ed511aee3a9e615ca06b4c39b
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-09-04 19:21:41 +0300

    Added IE11 page editor fix (wrong Sitecore browser detection)

---
+++ ScPlums/Fixes/Ie11UserAgentFixForSitecore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+
+namespace ScPlums.Fixes
+{
+    public class Ie11UserAgentFixForSitecore : HttpCapabilitiesDefaultProvider
+    {
+        public override HttpBrowserCapabilities GetBrowserCapabilities(HttpRequest request)
+        {
+            var browser = base.GetBrowserCapabilities(request);
+
+            // FIX: Sitecore still checks for "IE", but new ASP.NET returns "InternetExplorer"
+            if (browser.Browser == "InternetExplorer")
+            {
+                browser.Capabilities["browser"] = "IE";
+                // refresh cached value
+                typeof(HttpCapabilitiesBase)
+                    .GetField("_havebrowser", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .SetValue(browser, false);
+            }
+
+            return browser;
+        }
+    }
+}
---
+++ ScPlums/Initializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+using ScPlums.Fixes;
+
+namespace ScPlums
+{
+    public static class Initializer
+    {
+        public static void ApplyCodeFixes()
+        {
+            FixIE11SitecoreDetection();
+        }
+
+        private static void FixIE11SitecoreDetection()
+        {
+            HttpCapabilitiesBase.BrowserCapabilitiesProvider = new Ie11UserAgentFixForSitecore();
+        }
+    }
+}
