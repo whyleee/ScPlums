9de8cdf3c45b9f2cf17edeb4527bec61d5832f11
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-09-05 12:12:34 +0300

    Added fix to redirect back from login page in the page editor

---
+++ ScPlums/Fixes/RedirectBackFromLoginPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Pipelines.HttpRequest;
+using Sitecore.Sites;
+using Sitecore.Web;
+
+namespace ScPlums.Fixes
+{
+    public class RedirectBackFromLoginPage : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            var isEditOrPreviewMode = Sitecore.Context.PageMode.IsPageEditor || Sitecore.Context.PageMode.IsPreview;
+
+            if (isEditOrPreviewMode && !Sitecore.Context.User.IsAuthenticated)
+            {
+                var currentUrl = args.Context.Request.RawUrl;
+                var loginPageUrl = SiteContext.GetSite("shell").LoginPage;
+                var redirectUrl = string.Format("{0}?returnUrl={1}", loginPageUrl, currentUrl);
+
+                WebUtil.Redirect(redirectUrl, allowSame: false);
+            }
+        }
+    }
+}
