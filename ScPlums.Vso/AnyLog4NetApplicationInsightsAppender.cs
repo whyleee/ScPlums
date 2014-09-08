c47c49775cfd3e84a8d65d0ee0e7a59dbb97937c
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2014-09-08 12:28:51 +0300

    Added ScPlums.Vso project with fixed log4net appender for App-Insights tracing service

---
+++ ScPlums.Vso/AnyLog4NetApplicationInsightsAppender.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using log4net.Appender;
+using log4net.spi;
+using Microsoft.ApplicationInsights.Tracing;
+
+namespace ScPlums.Vso
+{
+    // Fixed version of MMA (Microsoft Monitoring Agent) log4net appender, which works with any log4net, including internal Sitecore version.
+    // TOUSE: register this appender in <log4net> config section instead of default 'Microsoft.ApplicationInsights.Log4NetAppender.ApplicationInsightsAppender'
+    public class AnyLog4NetApplicationInsightsAppender : AppenderSkeleton
+    {
+        private LoggingController loggingController;
+
+        /// <summary>
+        /// Get/set The Application Insights ComponentId for your application. 
+        /// </summary>
+        /// <remarks>
+        /// This is normally pushed from when appender is being initialized.
+        /// </remarks>
+        public string ComponentId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The <see cref="T:ScPlums.Vso.AnyLog4NetApplicationInsightsAppender" /> requires a layout.
+        /// This appender converts the LoggingEvent it receives into a text string and requires the layout format string to do so.
+        /// </summary>
+        protected override bool RequiresLayout
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the Appender and perform ComponentId validation.
+        /// </summary>
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+            try
+            {
+                loggingController = LoggingController.CreateLoggingController(ComponentId);
+            }
+            catch(Exception ex)
+            {
+                throw new LogException(ex.Message, ex);
+            }
+        }
+
+        /// <summary>
+        /// Append LoggingEvent Application Insights logging framework.
+        /// </summary>
+        /// <param name="loggingEvent">Events to be logged.</param>
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            try
+            {
+                loggingController.LogMessageWithData(base.RenderLoggingEvent(loggingEvent), loggingEvent);
+            }
+            catch(Exception ex)
+            {
+                throw new LogException(ex.Message, ex);
+            }
+        }
+
+        /// <summary>
+        /// Releases all resources used by <see cref="T:ScPlums.Vso.AnyLog4NetApplicationInsightsAppender" />.
+        /// </summary>
+        public override void OnClose()
+        {
+            if(loggingController != null)
+            {
+                loggingController.Dispose();
+                loggingController = null;
+            }
+            base.OnClose();
+        }
+    }
+}
---
+++ ScPlums.Vso/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+// General info about the product
+[assembly: AssemblyTitle("ScPlums.Vso")]
+[assembly: AssemblyDescription("Additional Sitecore plums for Visual Studio Online services")]
+[assembly: AssemblyCompany("Pavel Nezhencev (Creuna)")]
+[assembly: AssemblyProduct("ScPlums")]
+[assembly: AssemblyCopyright("Copyright © Pavel Nezhencev (Creuna) 2014")]
+
+// Product version
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
